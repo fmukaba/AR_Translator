650498f0e77526b83150e88ec242a2fbb32b2996
fxnolimit <fxkikomina@gmail.com>
2020-01-22 16:49:08 -0800

    init

---
+++ ARTranslator/ARTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARTranslator
+//
+//  Created by Francois Mukaba on 1/22/20.
+//  Copyright © 2020 Francois Mukaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
