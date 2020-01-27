72f91423d3ad3957b5ae580f8e72fa3d4876869f
Francois Mukaba <fxkikomina@gmail.com>
2020-01-27 13:08:08 -0800

    fixing google_plist

--- ARTranslator/ARTranslator/ViewController.swift
+++ ARTranslator/ARTranslator/ViewController.swift
@@ -7,8 +7,7 @@
 //
 
 import UIKit
-import ARKit
-import SceneKit
+
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
 
@@ -59,31 +58,4 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
-       // MARK: - ARSCNViewDelegate
-        
-    /*
-        // Override to create and configure nodes for anchors added to the view's session.
-        func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-            let node = SCNNode()
-         
-            return node
-        }
-    */
-        
-        func session(_ session: ARSession, didFailWithError error: Error) {
-            // Present an error message to the user
-            
-        }
-        
-        func sessionWasInterrupted(_ session: ARSession) {
-            // Inform the user that the session has been interrupted, for example, by presenting an overlay
-            
-        }
-        
-        func sessionInterruptionEnded(_ session: ARSession) {
-            // Reset tracking and/or remove existing anchors if consistent tracking is required
-            
-    }
 }
-
-
