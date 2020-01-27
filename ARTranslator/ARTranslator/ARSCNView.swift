556b0ec07ec1f1c7101e6daa1eb153598dc22f1a
Francois Mukaba <fxkikomina@gmail.com>
2020-01-27 11:51:27 -0800

    removed AR view

---
+++ ARTranslator/ARTranslator/ARSCNView.swift
@@ -0,0 +1,16 @@
+//
+//  ARSCNView.swift
+//  ARTranslator
+//
+//  Created by Francois Mukaba on 1/27/20.
+//  Copyright © 2020 Francois Mukaba. All rights reserved.
+//
+
+import Foundation
+
+class ARSCNView {
+    
+    
+    
+    
+}
--- ARTranslator/ARTranslator/ViewController.swift
+++ ARTranslator/ARTranslator/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
 
@@ -57,6 +59,31 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
-    
+       // MARK: - ARSCNViewDelegate
+        
+    /*
+        // Override to create and configure nodes for anchors added to the view's session.
+        func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+            let node = SCNNode()
+         
+            return node
+        }
+    */
+        
+        func session(_ session: ARSession, didFailWithError error: Error) {
+            // Present an error message to the user
+            
+        }
+        
+        func sessionWasInterrupted(_ session: ARSession) {
+            // Inform the user that the session has been interrupted, for example, by presenting an overlay
+            
+        }
+        
+        func sessionInterruptionEnded(_ session: ARSession) {
+            // Reset tracking and/or remove existing anchors if consistent tracking is required
+            
+    }
 }
+
 
