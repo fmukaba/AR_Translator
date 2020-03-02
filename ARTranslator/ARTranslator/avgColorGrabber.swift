9507aff50f4bf199ffb038ed28667d0359f443f1
Evan <45179103+evanj97@users.noreply.github.com>
2020-03-01 23:35:23 -0800

    average rect color working (?) test working

--- ARTranslator/ARTranslator/avgColorGrabber.swift
+++ ARTranslator/ARTranslator/avgColorGrabber.swift
@@ -11,6 +11,15 @@ import UIKit
 import CoreGraphics
 import CoreImage
 
+
+// INTENDED USAGE:
+
+// initialize an avgColorGrabber object with an image
+// make repeated calls to instanciated object with getAvgRectColor(rect: CGRect)
+// when finished with the image, free this object and create a new one OR:
+// call changeImage() to change the image.  changeImage() and init() are both compatible
+// with EITHER a UIImage or CGImage for convenience
+
 class avgColorGrabber: NSObject
 {
     var image: CGImage
@@ -18,15 +27,27 @@ class avgColorGrabber: NSObject
     var data: UnsafePointer<UInt8>
     //    var context: CGContext // needed?
 
-
     // Initialize with an image, either a UIImage or CGImage will work
-    public init(image: UIImage) // init with UIImage
+    init(image: UIImage) // init with UIImage
     {
         self.image = image.cgImage!
         pixelData = self.image.dataProvider!.data!
         data = CFDataGetBytePtr(pixelData)
     }
     init(image: CGImage) // init with CGImage
+    {
+        self.image = image
+        pixelData = self.image.dataProvider!.data!
+        data = CFDataGetBytePtr(pixelData)
+    }
+    
+    func changeImage(image: UIImage)
+    {
+        self.image = image.cgImage!
+        pixelData = self.image.dataProvider!.data!
+        data = CFDataGetBytePtr(pixelData)
+    }
+    func changeImage(image: CGImage)
     {
         self.image = image
         pixelData = self.image.dataProvider!.data!
@@ -46,4 +67,51 @@ class avgColorGrabber: NSObject
 
         return UIColor(red: r, green: g, blue: b, alpha: a)
     }
+    
+    func getAvgRectColor(rect: CGRect) -> UIColor
+    {
+        // create rectangle with INTEGER coords and dimensions
+        
+        var cleanRect = rect.standardized
+        
+        
+        print("\n\nDEBUG dimensions")
+        print("cleanRect.minX: \(cleanRect.minX)" )
+        print("cleanRect.maxX: \(cleanRect.maxX)" )
+        print("cleanRect.minY: \(cleanRect.minY)" )
+        print("cleanRect.maxY: \(cleanRect.maxY)" )
+        print("\n\n\n")
+        
+        let x0 = UInt32(cleanRect.minX)
+        let y0 = UInt32(cleanRect.minY)
+        let x1 = UInt32(cleanRect.maxX)
+        let y1 = UInt32(cleanRect.maxY)
+
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+
+        var count: UInt32 = 0
+        var pixelInfo: Int
+        
+        
+        for X in x0...x1
+        {
+            for Y in y0...y1
+            {
+                pixelInfo = ((Int(self.image.width) * Int(Y)) + Int(X)) * 4
+
+                r += CGFloat(data[pixelInfo]) / CGFloat(255.0)
+                g += CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+                b += CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+                a += CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+                
+                count += 1
+            }
+        }
+        
+        return UIColor(red: r/CGFloat(count), green: g/CGFloat(count), blue: b/CGFloat(count), alpha: a/CGFloat(count))
+    }
+    
 }
--- ARTranslator/ARTranslatorTests/ARTranslatorTests.swift
+++ ARTranslator/ARTranslatorTests/ARTranslatorTests.swift
@@ -22,15 +22,37 @@ class ARTranslatorTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("\n\n\nTEST TEST TEST TEST TEST TEST TEST TEST TEST\n")
+        
+        
+        let testImage = UIImage.init(named: "testImage_01.png")!
+        let test: avgColorGrabber = avgColorGrabber.init(image: testImage)
+
+        let testRect = CGRect.init(x: 140, y: 780, width: 60, height: 60)
+        print("AVG RECT COLOR: \(test.getAvgRectColor(rect: testRect))")
+        
+        print("\n\n\n")
+        print(test.getPixelColor(pos: CGPoint.init(x: 155, y: 888)))
+        print("\n\n\n")
     }
     
     func avgColorGrabber_getPixelColor()
     {
+        // THIS FUNC IS NOT RUNNING, BUT COPY-PASTING THE TEST INTO THE ABOVE
+        // FUNC WORKS, AND SO THERE IT SHALL REMAIN
         
-//        let test: avgColorGrabber = avgColorGrabber.init(image: UIImage.init(contentsOfFile: "/Users/Evan/Desktop/testImage_01.png")!)
+        // ALL GLORY TO THE HOLY CODE
+        // ALL GLORY TO HYPNOTOAD
+        
+//        print("TEST TEST TEST TEST TEST TEST TEST TEST TEST")
 //
+//        let testImage = UIImage.init(named: "testImage_01.png")!
+//        let test: avgColorGrabber = avgColorGrabber.init(image: testImage)
+//
+//        print("\n\n\n")
 //        print(test.getPixelColor(pos: CGPoint.init(x: 1, y: 1)))
-        
+//        print("\n\n\n")
+
     }
 
     func testPerformanceExample() {
