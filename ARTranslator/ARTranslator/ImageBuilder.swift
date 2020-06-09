d0b70c5ba020d94ca5a02a53dbdb3c7bd58aa9c5
Evan <45179103+evanj97@users.noreply.github.com>
2020-06-09 11:54:39 -0700

    Initial commit of ImageBuilder.swift and ImageElementAdder.swift

---
+++ ARTranslator/ARTranslator/ImageBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ImageBuilder.swift
+//  ARTranslator
+//
+//  Created by Evan Johnson on 08/6/20.
+//  Copyright © 2020 Francois Mukaba. All rights reserved.
+//
+
+import Foundation
---
+++ ARTranslator/ARTranslator/ImageElementAdder.swift
@@ -0,0 +1,9 @@
+//
+//  ImageElementAdder.swift
+//  ARTranslator
+//
+//  Created by Evan Johnson on 08/6/20.
+//  Copyright © 2020 Francois Mukaba. All rights reserved.
+//
+
+import Foundation
